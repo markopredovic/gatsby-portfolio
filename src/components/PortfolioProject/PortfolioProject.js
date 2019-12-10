7016f25317d5bd464580a74363e3f5cbc9350bf2
Marko Predovic <mpredovic@gmail.com>
2019-12-10 15:02:33 +0100

    animation added

--- src/components/PortfolioProject/PortfolioProject.js
+++ src/components/PortfolioProject/PortfolioProject.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const PortfolioProject = ({ node, showDescription, activeProjectType }) => {
   return (
-    <div className="l-portfolio-project">
+    <div className="l-portfolio-project animated fadeIn">
       <div className="l-img">
         <a href={node.liveUrl} target="_blank">
           <img src={node.projectImage.file.url} alt={node.title} />
