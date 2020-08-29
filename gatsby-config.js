9b09e228ef1a41f9868ff5e81d630336c5dddf7f
Marko Predovic <mpredovic@gmail.com>
2020-08-29 12:46:13 +0200

    upgrade all packages to latest versions

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,21 +38,6 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          "gatsby-remark-relative-images",
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
-            },
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `GatsbyJS`,
