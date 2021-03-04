43c93eea0e60dd947b2716c68ed33528c2133dca
Marko Predovic <mpredovic@gmail.com>
2021-03-04 05:42:35 +0100

    post page

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import Img from "gatsby-image"
 import { BLOCKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import Layout from "../components/Layout/layout"
-import { FaArrowAltCircleLeft } from "react-icons/fa"
+import { Box, defaultProps, Heading } from "grommet"
+import Container from "../components/UI/Container"
+import { Return } from "grommet-icons"
 
 export const query = graphql`
   query($slug: String!) {
@@ -37,36 +39,81 @@ const blog = (props) => {
   }
 
   return (
-    <Layout bodyClass={props.pageContext.bodyClass}>
-      <div className="l-main-content l-post">
-        <div className="l-header">
-          <h3 className="m-post-title">
-            {props.data.contentfulBlogPost.title}
-          </h3>
-          <h4 className="m-post-subtitle">
-            {props.data.contentfulBlogPost.subtitle}
-          </h4>
-        </div>
-        <div className="l-img">
+    <Box>
+      <TitleBox>
+        <Heading level="3" fill={true}>
+          {props.data.contentfulBlogPost.title}
+        </Heading>
+        <Heading level="4" fill={true}>
+          {props.data.contentfulBlogPost.subtitle}
+        </Heading>
+      </TitleBox>
+      <Container>
+        <ImageBox>
           <Img
             fluid={props.data.contentfulBlogPost.featuredImage.fluid}
             alt={props.data.contentfulBlogPost.title}
           />
-        </div>
-        <div className="l-content">
+        </ImageBox>
+        <ContentBox>
           {documentToReactComponents(
             props.data.contentfulBlogPost.body.json,
             options
           )}
-        </div>
-        <div className="l-back">
+        </ContentBox>
+        <BackBox>
           <Link to="/blog">
-            <FaArrowAltCircleLeft /> back to blog
+            <Return /> back to blog
           </Link>
-        </div>
-      </div>
-    </Layout>
+        </BackBox>
+      </Container>
+    </Box>
   )
 }
 
 export default blog
+
+const TitleBox = styled(Box)`
+  padding: 30px 10px;
+  margin-bottom: 20px;
+  text-align: center;
+  color: #fff;
+  background-color: ${defaultProps.theme.global.colors["brand"]};
+
+  h3 {
+    margin-bottom: 20px;
+    font-size: 2.4rem;
+  }
+  h4 {
+    margin-bottom: 0;
+    font-size: 1.8rem;
+  }
+
+  @media (min-width: 992px) {
+    margin-bottom: 40px;
+
+    h3 {
+      font-size: 2.8rem;
+    }
+    h4 {
+      font-size: 2rem;
+    }
+  }
+`
+
+const ImageBox = styled(Box)`
+  margin-bottom: 40px;
+`
+
+const ContentBox = styled(Box)``
+
+const BackBox = styled(Box)`
+  a {
+    display: inline-flex;
+    align-items: center;
+
+    svg {
+      margin-right: 4px;
+    }
+  }
+`
